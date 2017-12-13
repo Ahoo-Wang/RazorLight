72c9a48b4673be00508b2682a949301988fb505c
Ahoo Wang <ahoowang@qq.com>
2017-12-13 21:18:26 +0800

    1. fix RazorLightEngine.CompileRenderAsync set ViewBag throw bug!

--- sandbox/RazorLight.Sandbox/Program.cs
+++ sandbox/RazorLight.Sandbox/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Dynamic;
 
 namespace RazorLight.Sandbox
 {
@@ -13,13 +14,14 @@ namespace RazorLight.Sandbox
         private static async Task MainAsync()
         {
             var engine = new EngineFactory().ForEmbeddedResources(typeof(Program));
-
+          
             var model = new
             {
                 Name1 = "qwe"
             };
-
-            string s = await engine.CompileRenderAsync("go", model, model.GetType(), null);
+            dynamic viewBag = new ExpandoObject();
+            viewBag.Ahoo = "Great Man!";
+            string s = await engine.CompileRenderAsync("go", model, model.GetType(), viewBag);
 
             Console.WriteLine(s);
             Console.ReadKey();
